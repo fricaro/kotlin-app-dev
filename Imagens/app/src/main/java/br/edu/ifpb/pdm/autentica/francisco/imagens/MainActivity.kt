993dabb572a208ee7fde1ea049a03125178f8762
Francisco <fricaro@live.com>
2020-03-08 21:50:11 -0300

    Add: private keyword to functions when needed

--- Imagens/app/src/main/java/br/edu/ifpb/pdm/autentica/francisco/imagens/MainActivity.kt
+++ Imagens/app/src/main/java/br/edu/ifpb/pdm/autentica/francisco/imagens/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         this.btDownload.setOnClickListener{ download() }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override  fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (resultCode == Activity.RESULT_OK){
@@ -49,23 +49,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun camera(){
+    private fun camera(){
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         startActivityForResult(intent, CAMERA)
     }
 
-    fun xml(){
+    private fun xml(){
         this.ivImagem.setImageResource(R.drawable.super_mario)
     }
 
-    fun downloadDaImagem(url: String?) : Bitmap{
+    private fun downloadDaImagem(url: String?) : Bitmap{
         URL(url).openStream().use {
             val imagem = BitmapFactory.decodeStream(it)
             return imagem
         }
     }
 
-    fun createSmHandler(formato: String?) {
+    private fun createSmHandler(formato: String?) {
         val handler = Handler()
         val url = "http://www.valeria.eti.br/sm/sm_${formato}.png"
         Thread {
@@ -77,14 +77,13 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
-    fun download(){
+    private fun download(){
         var selection: String? = null
         val resolutions = resources.getStringArray(R.array.sm_resolution)
         AlertDialog.Builder(this)
             .setTitle("Escolha um tamanho.")
             .setSingleChoiceItems(resolutions,0) { dialogInterface, which ->
                 selection = resolutions[which]
-
             }
             .setPositiveButton("Ok") { dialog, i ->
                 createSmHandler(selection)
